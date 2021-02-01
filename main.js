11fabc8e09d6561e1c8c658b6a0bebe5180c1622
shou yamamoto <yamasyo0017@gmail.com>
2021-02-01 23:45:49 +0900

    handson work6

--- main.js
+++ main.js
@@ -2,15 +2,17 @@
 const ul = document.querySelector('ul');
 const fragment = document.createDocumentFragment();
 
-const p = Promise.resolve(
-  [{to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"}, {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}],
-);
-
-p.then(function(items) {
-  createElement(items);
+const p = new Promise(function(resolve) {
+  setTimeout(function() {
+    resolve([{to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"}, {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}])
+  }, 3000);
 });
 
-function createElement(items) {
+p.then(function(items) {
+  createElements(items);
+});
+
+function createElements(items) {
   for(const item of items) {
     const li = document.createElement('li');
     const a = document.createElement('a');
@@ -26,20 +28,3 @@ function createElement(items) {
   }
   ul.appendChild(fragment);
 }
-
-
-// const fragment = document.createDocumentFragment();
-// for(const item of items) {
-//   const li = document.createElement('li');
-//   const a = document.createElement('a');
-//   const img = document.createElement('img');
-//   const text = item.text;
-//   a.href = item.to;
-//   img.alt = item.alt;
-//   img.src = item.img;
-//   a.appendChild(img);
-//   a.insertAdjacentHTML('beforeend', text);
-//   li.appendChild(a);
-//   fragment.appendChild(li);
-// }
-// ul.appendChild(fragment);
