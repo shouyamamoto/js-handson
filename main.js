50d356c268ab0c37950d38c35fb4adfab5c5a429
shou yamamoto <yamasyo0017@gmail.com>
2021-02-01 23:00:37 +0900

    handson work3

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 'use strict';
-
 const ul = document.querySelector('ul');
-const li = document.createElement('li');
-const img = document.createElement('img');
-const a = document.createElement('a');
-a.href = '1.html';
-img.src = 'bookmark.png';
-img.alt = 'ブックマーク';
 
+const texts = ['a1', 'a2'];
+const hrefs = ['a1.html', 'a2.html'];
+const srcs = ['/img/bookmark.png', '/img/bookmark.png'];
 
-ul.appendChild(li);
-li.appendChild(a);
-a.appendChild(img);
-a.insertAdjacentHTML('beforeend', 'これです');
+const fragment = document.createDocumentFragment();
+
+for(let i = 0; i < texts.length; i++) {
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+  const img = document.createElement('img');
+  const text = texts[i];
+  a.href = hrefs[i];
+  img.src = srcs[i];
+  a.appendChild(img);
+  a.insertAdjacentHTML('beforeend', text);
+  li.appendChild(a);
+  fragment.appendChild(li);
+}
+ul.appendChild(fragment);
 
