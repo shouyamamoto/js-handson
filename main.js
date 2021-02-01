5162333c77d7828a7463974fd3a73db3268acd76
shou yamamoto <yamasyo0017@gmail.com>
2021-02-02 08:15:32 +0900

    handson work9

--- main.js
+++ main.js
@@ -7,24 +7,20 @@ loading.src = 'loading-circle.gif';
 loading.className = 'loading-circle';
 ul.appendChild(loading);
 
-const p = new Promise(function(resolve, reject) {
-  if(false) { // 本当はここにtrueになる条件を書く
-    setTimeout(function() {
-      resolve([{to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"}, {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}])
-    }, 3000);
-  } else {
-    setTimeout(function() {
-      reject();
-    },3000) 
-  }
+const p = function() {
+  return new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    resolve([{to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"}, {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}])
+  }, 3000);
 });
+}
 
-p.then(function(items) {
+async function a() {
+  const result = await p();
   loading.remove();
-  createElements(items);
-}).catch(function() {
-  console.log('データが取得できませんでした。');
-});
+  await createElements(result)
+};
+a();
 
 function createElements(items) {
   for(const item of items) {
@@ -35,8 +31,8 @@ function createElements(items) {
     a.href = item.to;
     img.alt = item.alt;
     img.src = item.img;
-    a.appendChild(img);
     a.insertAdjacentHTML('beforeend', text);
+    a.appendChild(img);
     li.appendChild(a);
     fragment.appendChild(li);
   }
