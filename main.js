713f25ad6362052db8fe8513d4314fef661b35f5
shou yamamoto <yamasyo0017@gmail.com>
2021-02-03 07:12:06 +0900

    handson work11 try-catch-fainallyの修正

--- main.js
+++ main.js
@@ -7,28 +7,18 @@ loading.src = 'loading-circle.gif';
 loading.className = 'loading-circle';
 ul.appendChild(loading);
 
-// const p = function() {
-//   return new Promise(function(resolve, reject) {
-//   setTimeout(function() {
-//     resolve([{to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"}, {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}])
-//   }, 3000);
-// });
-// }
-
 (async function() {
   try {
     const response = await fetch('https://jsondata.okiba.me/v1/json/9omPz210202144336');
     const json = await response.json();
     const data = await json.data;
-    return data;
+    setTimeout(createElements.bind(null, data) ,3000);
   }catch(e) {
     console.error(e);
   }finally {
     setTimeout(removeLoadingIcon, 3000);
   }
-})().then(function(data) {
-  setTimeout(createElements.bind(null, data), 3000);
-});
+})();
 
 function removeLoadingIcon() {
   loading.remove();
