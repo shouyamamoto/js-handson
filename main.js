9f2b36514c900f6177a44214abb654838f28221c
shou yamamoto <yamasyo0017@gmail.com>
2021-02-05 22:30:15 +0900

    handson work13 fetchData修正

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ const loading = document.createElement('img');
 loading.src = 'loading-circle.gif';
 loading.className = 'loading-circle';
 
-const reqBtn = document.querySelector('.req-btn');
-const modalBtn = document.querySelector('.modal-btn');
-const mask = document.querySelector('.mask');
+const reqBtn = document.querySelector('#js-req-btn');
+const modalBtn = document.querySelector('#js-modal-btn');
+const mask = document.querySelector('#js-mask');
 
 mask.addEventListener('click', () => {
   if(mask.classList.contains('active')) {
@@ -22,25 +22,32 @@ modalBtn.addEventListener('click', () => {
   mask.classList.toggle('active');
 });
 
-reqBtn.addEventListener('click', () => {
+reqBtn.addEventListener('click', async () => {
   ul.appendChild(loading);
-  setTimeout(fetchData, 3000);
-
   modalBtn.remove();
   mask.remove();
   reqBtn.remove();
+
+  // thenのreturnで新しいpromiseが返ってきているので、awaitで非同期処理を待つ
+  const data = await fetchData();
+  createElements(data);
 });
 
 async function fetchData() {
   try {
-    const response = await fetch('https://jsondata.okiba.me/v1/json/9omPz210202144336');
-    const json = await response.json();
-    loading.remove();
-    createElements(json.data);
+    const fetchResult = await fetch('https://jsondata.okiba.me/v1/json/9omPz210202144336')
+    .then((response) => {
+      // ここでawaitを使わず、thenを使うのは、response.json()の実行が非同期処理ではないから？
+      // でもfetchからの処理をまってからthenは実行される。
+      return response.json();
+    });
+    console.log(fetchResult.data);
+    // thenの中でreturnした値が、fetchDataを実行したときに返却される値になる。（新しいpromiseを返す）
+    return fetchResult.data;
   } catch(e) {
     console.error(e);
   } finally {
-    console.log('fetchData run');
+    loading.remove();
   }
 }
 
