4f3ae24e04e0f842bbf367e583aad7c845094d56
shou yamamoto <yamasyo0017@gmail.com>
2021-02-05 23:54:25 +0900

    handson work15

--- main.js
+++ main.js
@@ -1,46 +1,60 @@
 'use strict';
 const ul = document.querySelector('ul');
 const fragment = document.createDocumentFragment();
-
 const loading = document.createElement('img');
+const submitBtn = document.querySelector('#js-submit');
+const modalBtn = document.querySelector('#js-modal-btn');
+const mask = document.querySelector('#js-mask');
+const inputNum = document.querySelector('#js-input-num');
+const inputName = document.querySelector('#js-input-name');
+const form = document.querySelector('#js-form');
+
+// ローディング画像表示
 loading.src = 'loading-circle.gif';
 loading.className = 'loading-circle';
 
-const reqBtn = document.querySelector('.req-btn');
-const modalBtn = document.querySelector('.modal-btn');
-const mask = document.querySelector('.mask');
+// モーダルを表示させる処理
+modalBtn.addEventListener('click', () => {
+  submitBtn.classList.add('active');
+  form.classList.add('active');
+  inputNum.classList.add('active');
+  mask.classList.add('active');
+});
+
+// リクエストボタンを押したあとの処理
+submitBtn.addEventListener('click', async () => {
+  console.log(inputNum.value);
+  console.log(inputName.value);
+
+  ul.appendChild(loading);
+  modalBtn.remove();
+  form.remove();
+  mask.remove();
+  submitBtn.remove();
+
+  const data = await fetchData();
+  createElements(data);
+});
 
 mask.addEventListener('click', () => {
-  if(mask.classList.contains('active')) {
-    mask.classList.remove('active');
-    reqBtn.classList.remove('active');
-  }
-});
-
-modalBtn.addEventListener('click', () => {
-  reqBtn.classList.toggle('active');
-  mask.classList.toggle('active');
-});
-
-reqBtn.addEventListener('click', () => {
-  ul.appendChild(loading);
-  setTimeout(fetchData, 3000);
-
-  modalBtn.remove();
-  mask.remove();
-  reqBtn.remove();
+  submitBtn.classList.remove('active');
+  form.classList.remove('active');
+  inputNum.classList.remove('active');
+  inputName.classList.remove('active');
+  mask.classList.remove('active');
 });
 
 async function fetchData() {
   try {
-    const response = await fetch('https://jsondata.okiba.me/v1/json/9omPz210202144336');
-    const json = await response.json();
-    loading.remove();
-    createElements(json.data);
+    const fetchResult = await fetch('https://jsondata.okiba.me/v1/json/9omPz210202144336')
+    .then(response => {
+      return response.json();
+    });
+    return fetchResult.data;
   } catch(e) {
     console.error(e);
   } finally {
-    console.log('fetchData run');
+    loading.remove();
   }
 }
 
