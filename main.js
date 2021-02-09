80ca12627af55bbe6aa696f889db85e311ea784a
shou yamamoto <yamasyo0017@gmail.com>
2021-02-09 15:59:28 +0900

    handson work17 機能追加完了

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const today = new Date('2021/2/9')
+
 const body = document.querySelector('body')
 const tabs = document.querySelector('ul')
 tabs.classList.add('tab')
@@ -26,6 +28,11 @@ const contentsInners = [newsContentsInner, economyContentsInner, entertainmentCo
 const titleWraps = [newsTitleWrap, economyTitleWrap, entertainmentTitleWrap, sportsTitleWrap, japanTitleWrap]
 
 contentsWrap.classList.add('contentWrap')
+newsContents.id = "news"
+economyContents.id = "economy"
+entertainmentContents.id = "entertainment"
+sportsContents.id = "sports"
+japanContents.id = "japan"
 
 for(let i = 0; i < contents.length; i++) {
   contentsWrap.appendChild(contents[i])
@@ -35,63 +42,115 @@ for(let i = 0; i < contents.length; i++) {
   body.appendChild(contentsWrap)
 }
 
+
+
+
 async function fetchArticleData () {
   try {
-    const articleData = await fetch('https://jsondata.okiba.me/v1/json/BPyzv210207140601')
+    const articleData = await fetch('https://jsondata.okiba.me/v1/json/WCTTJ210208232654')
     .then(response => {
       const json =  response.json()
       return json
     })
-    .then(json => {
-      const articles = json.articleList
-      const categoryNames = new Set()
-      const imgSources = new Set()
-      const articleData = new Set()
-      for(article of articles) {
-        const categoryName = article.category
-        const imgSrc = article.img
-        const articles = article.articleInfo
-        categoryNames.add(categoryName)
-        imgSources.add(imgSrc)
-        articleData.add(articles)
-      }
-      return {
-        categoryNames, imgSources, articleData, articles
-      }
-    })
-    return articleData
+    return articleData.articleList
   } catch(e) {
-    console.log('ぶっこわれてます')
+    alert('ぶっこわれてます')
   } finally {
     console.log('fetchData run')
   }
 }
 
 async function createElement () {
-  const articleData = await fetchArticleData()
+  const articleList = await fetchArticleData()
+  const tab_list = new Set()
 
-  // tabの生成
-  for(const categoryName of articleData.categoryNames) {
+  for(const article of articleList) {
+    // タブの生成
     const tab = document.createElement('li')
+    tab.textContent = article.category
     tab.classList.add('tab__item')
-    tab.textContent = categoryName
     tabs.appendChild(tab)
-  }
-  // タイトルの生成
-  for(const articleArray of articleData.articleData) {
-    for(const article of articleArray) {
+    tab_list.add(tab)
+
+    // どのタブを初期表示にするか
+    if(article.firstView === 'true') {
+      tab.classList.add('active')
+    }
+
+    if(article.category === 'ニュース') {
+      tab.dataset.id = 'news'
+    } else if(article.category === '経済') {
+      tab.dataset.id = 'economy'
+    } else if(article.category === 'エンタメ') {
+      tab.dataset.id = 'entertainment'
+    } else if(article.category === 'スポーツ') {
+      tab.dataset.id = 'sports'
+    } else if(article.category === '国内') {
+      tab.dataset.id = 'japan'
+    }
+
+    if(article.category === 'ニュース' && article.firstView === 'true') {
+      newsContents.classList.add('active')
+    } else if(article.category === '経済') {
+      economyContents.classList.add('active')
+    } else if(article.category === 'エンタメ') {
+      entertainmentContents.classList.add('active')
+    } else if(article.category === 'スポーツ') {
+      sportsContents.classList.add('active')
+    } else if(article.category === '国内') {
+      japanContents.classList.add('active')
+    }
+    // タブの生成ここまで
+
+    // タイトルの生成
+    for(const info of article.articleInfo) {
       const title = document.createElement('li')
-      const titleLink = document.createElement('a')
-      titleLink.href = "#"
-      titleLink.textContent = article.title
-      title.appendChild(titleLink)
+      const title_link = document.createElement('a')
+      const comment = document.createElement('span')
+      const comment_icon = document.createElement('img')
       title.classList.add('title')
+      comment.classList.add('comment')
+      title_link.innerHTML = info.title
+      comment_icon.src = 'img/comment.png'
+      comment_icon.classList.add('comment_icon')
+
+      title_link.appendChild(comment)
+      title.appendChild(title_link)
+
+      // 投稿日と2021/2/9との日差を取得
+      const postDay = new Date(info.postDay)
+      const ms = today.getTime() - postDay.getTime()
+      const days = Math.floor(ms / (1000*60*60*24))
+
+      // 14日以内の投稿であればnew_iconを追加する
+      if(days <= 14) {
+        const new_icon = document.createElement('img')
+        new_icon.src = 'img/new_icon.png'
+        new_icon.classList.add('new_icon')
+
+        if(article.category === 'ニュース') {
+          title.appendChild(new_icon)
+        } else if(article.category === '経済') {
+          title.appendChild(new_icon)
+        } else if(article.category === 'エンタメ') {
+          title.appendChild(new_icon)
+        } else if(article.category === 'スポーツ') {
+          title.appendChild(new_icon)
+        } else if(article.category === '国内') {
+          title.appendChild(new_icon)
+        } 
+      }
+
+      // コメントがあればアイコンを追加
+      if(info.comment > 0) {
+        comment.textContent = info.comment
+        comment.appendChild(comment_icon)
+      }
 
       if(article.category === 'ニュース') {
         newsTitleWrap.appendChild(title)
         newsContentsInner.appendChild(newsTitleWrap)
         newsContents.appendChild(newsContentsInner)
-        newsContents.classList.add('active')
       } else if(article.category === '経済') {
         economyTitleWrap.appendChild(title)
         economyContentsInner.appendChild(economyTitleWrap)
@@ -110,26 +169,51 @@ async function createElement () {
         japanContents.appendChild(japanContentsInner)
       } 
     }
-  }
+    // タイトルの生成ここまで
 
-  // 画像の生成
-  for(const imgSrc of articleData.imgSources) {
+    // 画像の生成
     const img = document.createElement('img')
-    img.src = imgSrc
+    img.src = article.img
     img.classList.add('img')
 
-    if(imgSrc === 'img/news-image.png') {
+    if(article.category === 'ニュース') {
       newsContentsInner.appendChild(img)
-    } else if(imgSrc === 'img/economy-image.png') {
+      newsContents.appendChild(newsContentsInner)
+    } else if(article.category === '経済') {
       economyContentsInner.appendChild(img)
-    } else if(imgSrc === 'img/entertainment-image.png') {
+      economyContents.appendChild(economyContentsInner)
+    } else if(article.category === 'エンタメ') {
       entertainmentContentsInner.appendChild(img)
-    } else if(imgSrc === 'img/sports-image.png') {
+      entertainmentContents.appendChild(entertainmentContentsInner)
+    } else if(article.category === 'スポーツ') {
       sportsContentsInner.appendChild(img)
-    } else if(imgSrc === 'img/japan-image.png') {
+      sportsContents.appendChild(sportsContentsInner)
+    } else if(article.category === '国内') {
       japanContentsInner.appendChild(img)
+      japanContents.appendChild(japanContentsInner)
     }
+    // 画像の生成ここまで
   }
+
+  tabClickAction(tab_list)
+}
+
+function tabClickAction (tabList) {
+  tabList.forEach(clickedTab => {
+    clickedTab.addEventListener('click', () => {
+      tabList.forEach(tab => {
+        tab.classList.remove('active')
+      })
+
+      clickedTab.classList.add('active')
+
+      contents.forEach(content => {
+        content.classList.remove('active')
+      })
+
+      document.getElementById(clickedTab.dataset.id).classList.add('active')
+    })
+  })
 }
 
 createElement()
