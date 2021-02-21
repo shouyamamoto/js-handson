78adfa3ef5d9b5ce006c81f9be5628c54b60ffb4
shou yamamoto <yamasyo0017@gmail.com>
2021-02-22 06:56:16 +0900

    test commit

--- slide.js
+++ slide.js
@@ -38,7 +38,7 @@ async function createImg() {
   } catch(e) {
     console.error(e);
   } finally {
-    console.log('createImg run');
+    console.log('createImg Run');
   }
   return slideImages
 }
